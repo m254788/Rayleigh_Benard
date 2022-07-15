a7cfacfeb3224fad20468a0a329ab15beb25c6e3
m254788 <m254788@usna.edu>
2022-07-15 13:02:04 -0500

    fix error in GS check

--- check_against_gold.py
+++ check_against_gold.py
@@ -3,9 +3,17 @@ import sys
 gs_name = sys.argv[1]
 check_name = sys.argv[2]
 
+# ly51_T10000:
+# ly = 51
+# aspect_ratio = 2
+# Ra = 100000
+# Thot = 1.
+# Tcold = 0
+# maxT = 10000
+
 GOLD_STANDARD = np.loadtxt(gs_name,dtype=np.float64)
 check_board = np.loadtxt(check_name,dtype=np.float64)
 
 
 dif = check_board-GOLD_STANDARD
-print(np.linalg.norm(dif,ord=1)/np.sum(GOLD_STANDARD))	
+print(np.linalg.norm(dif,ord=1)/np.linalg.norm(GOLD_STANDARD,ord=1))	
